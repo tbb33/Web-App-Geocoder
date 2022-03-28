2b7d1a6cc1472c562cd62655dc43e552e442f565
tbb33 <91215510+tbb33@users.noreply.github.com>
2022-03-27 21:41:33 -0500

    Adding Lat Col
    Using OpenStreetMap API to append latitude column

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, send_file
 from werkzeug.utils import secure_filename
 from csv import writer, reader
+import urllib
+import requests
 
 app = Flask(__name__)
 
@@ -15,16 +17,24 @@ def success():
     file.save(secure_filename("uploaded"+file.filename))
 
     with open("uploaded"+file.filename, "r") as read_file,\
-    open("ooutput.csv", "w",newline='') as write_file:
+    open("output.csv", "w",newline='') as write_file:
         csv_reader = reader(read_file)
         csv_writer = writer(write_file)
-        print(csv_reader.line_num)
+
         for row in csv_reader:
             #new column
             if csv_reader.line_num == 1: #header
                 row.append("Latitude")
             else:
-                row.append(row[0]+'a') #first row append "a"
+                #row.append(row[1].latitude)
+                #row.append(row[1].apply(lambda loc: tuple(loc.point) if loc else None))
+                #row.append(row[0]+'a') #first row append "a"
+                url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(row[1]) +'?format=json'
+                response = requests.get(url).json()
+                if(len(response)!=0):
+                    row.append(response[0]['lat'])
+                else:
+                    return('-1')
             csv_writer.writerow(row)
 
 
